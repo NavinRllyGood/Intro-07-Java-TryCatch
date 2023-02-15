66571d7a6e43902cccb23c525d32947ae9428a50
Navin balekomebole <navin.balekomebole@stu.ocsb.ca>
2023-02-15 19:30:00 +0000

    Added TryCatch.java

--- TryCatch.java
+++ TryCatch.java
@@ -1,16 +1,25 @@
-
 import java.util.Scanner;
 
 /**
 * Java program to demonstrate working try catch in Java.
 *
-*
-* @author  Navin
+* @author Navin
 * @version 1.0
-* @since   2023-02-14
+* @since 2023-02-14
 */
 
 public final class TryCatch {
+    /**
+    * Main lines of code.
+    *
+    *
+    * @exception IllegalStateException Utility Class
+    * @see IllegalStateException
+    */
+    private TryCatch() {
+        throw new IllegalStateException("Utility Class");
+    }
+
     /**
     * Main lines of code.
     *
@@ -24,32 +33,31 @@ public final class TryCatch {
 
         final Scanner scanner = new Scanner(System.in);
 
-        final String someString = scanner.nextLine();
+        // get the radius as a string
+        final String radiusString = scanner.nextLine();
 
-        System.out.println(" " + someString);
-
-        // Closing Scanner
-
-        scanner.close();
         try {
             // Convert the string to a double
-            final double radDouble = Double.parseDouble(someString);
+            final double radDouble = Double.parseDouble(radiusString);
 
+            // check if negative
             if (radDouble < 0) {
                 System.out.println("This is a negative number");
+            } else {
+                // calculate the volume
+                final double volume = (4.0 / 3.0)
+                    * Math.PI * Math.pow(radDouble, 3);
+
+                system.out.print("The volume of your sphere");
+                System.out.format(" is %.2f m^3.", volume);
+                System.out.println("");
             }
-
-            final double volume = (4.0 / 3.0)
-                * Math.PI * Math.pow(radDouble, 3);
-
-            System.out.format("The volume of your sphere is %.2f m^3.", volume);
-            System.out.println("");
         } catch (NumberFormatException error) {
             System.out.println("Please enter a valid radius. "
                 + error.getMessage());
         }
 
         // Closing Scanner
+        scanner.close();
     }
-
 }
